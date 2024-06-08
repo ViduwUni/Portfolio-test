c2ace36531156cd04861e1cb9b478907b87e78c1
ViduwUni <168211986+ViduwUni@users.noreply.github.com>
2024-06-08 09:50:25 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+function showSidebar()
+{
+    const sidebar = document.querySelector('.side-nav')
+    sidebar.style.display = 'flex' 
+}
+
+function hideSidebar()
+{
+    const sidebar = document.querySelector('.side-nav')
+    sidebar.style.display = 'none'
+}
